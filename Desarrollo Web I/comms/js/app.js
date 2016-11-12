f606772c982a205838b3d99d18d5abccc5e80284
Luigi Rojas <xluigix98@gmail.com>
2016-11-12 12:18:10 -0400

    update 12 nov 2916

--- Desarrollo Web I/comms/js/app.js
+++ Desarrollo Web I/comms/js/app.js
@@ -1,71 +1,60 @@
-var id = document.getElementById('id'),
+var username = document.getElementById('username'),
+    password = document.getElementById('password'),
     namae = document.getElementById('namae'),
     lastname = document.getElementById('lastname'),
-    address = document.getElementById('address'),
+    email = document.getElementById('email'),
     save = document.getElementById('save'),
     list = document.getElementById('list'),
-    registro;
+    registro,
+    userData;
 
- function fetchRegisters(){
-   let XHR = new XMLHttpRequest();
-   XHR.open('get', 'https://glacial-eyrie-58697.herokuapp.com/', 'true');
-   XHR.onload = fetchRegistersSuccessful;
-   XHR.send();
- }
+function fetchRegisters() {
+    let XHR = new XMLHttpRequest();
+    XHR.open('get', 'http://192.168.0.111:8080/paydaii/app/api/public/users', 'true');
+    XHR.onload = fetchRegistersSuccessful;
+    XHR.send();
+}
 
- function fetchRegistersSuccessful(response){
+function saveUser(){
+    let XHR = new XMLHttpRequest();
+    XHR.open('post', 'http://192.168.0.111:8080/paydaii/app/api/public/user', 'true');
+    XHR.onload = saveUserSuccess;
+    userData = {
+        username: username.value,
+        password: password.value,
+        name: namae.value,
+        lastname: lastname.value,
+        email: email.value,
+        birthday: birthday.value
+    };
+    XHR.setRequestHeader('Content-Type', 'application/json');
+    XHR.send(JSON.stringify(userData));
+    console.log(userData);
+}
+
+function fetchRegistersSuccessful(response) {
+    console.log(response.target);
     registro = JSON.parse(response.target.response);
     console.log(registro);
-    render();
+    render(registro.users);
 }
 
-function render(){
-   console.log('fuck');
-   let tablebody = document.querySelector('#listTable tbody');
-   registro.forEach(function(registros, index){
-      tablebody.insertRow(index);
+function saveUserSuccess(r){
+    r = r.target;
+    if (r.status === 200){
+        Materialize.toast('I see you... ;)', 3000);
+        render(r.users);
+    } else {
+        Materialize.toast('Oops!', 3000);
+    }
 
-      for(let i=0; i < 5; i++){
-         tablebody.rows[index].insertCell(i);
-      }
-      tablebody.row[index].cells[0].textContent = registro.cedula;
-      tablebody.rows[index].cells[1].textContent = registro.nombres;
-      tablebody.rows[index].cells[2].textContent = registro.apellidos;
-      tablebody.rows[index].cells[3].textContent = registro.fecha_nacimiento;
-      tablebody.rows[index].cells[4].textContent = registro.dirección;
-      console.log(index);
-   });
+}
+
+function render(registro) {
+    registro.forEach(function(user) {
+        let p = document.createElement('p')
+        p.textContent = `Username ${user.us_user}`;
+        document.body.appendChild(p);
+    })
 }
 fetchRegisters();
-
-function calculate(){
-   var a = id.value;
-   var b = namae.value;
-   console.log(id.value);
-   console.log(namae.value);
-   var err = [1];
-   var root = [];
-   var lel = true;
-   var i = 0;
-
-   for (i = 0; err[i] >= 0.01 ; i++) {
-      root[i] = (a + b)/2;
-      console.log(root[i]);
-
-      if (err[i-1] !== undefined) {
-         err[i] = ((root[i] - root[i-1])/ root[i]) * 100;
-         console.log(err[i]);
-      } else {
-
-      }
-      if(lel){
-         a = root[i];
-         lel = false;
-      } else {
-         b = root[i];
-         lel = true;
-      }
-   }
-}
-
-save.onclick = calculate;
--- Desarrollo Web I/comms/readme.md
+++ Desarrollo Web I/comms/readme.md
@@ -1,3 +1,6 @@
 #Descripción
-Producir una aplicación web que permita el registrdo de usuarios empleando endpoints en un servidor web.
+Producir una aplicación web que permita el registro de usuarios empleando endpoints en un servidor web.
 Tambien debe desplegar a los usuarios registrados en una tabla.
+
+1- Recuperar los datos del usuario empleando el método GET para consumir el recurso.
+2- Cargar los datos escritos por el usuario empleando POST y determinado si el resultado de la operación fue satisfactorio o no.
